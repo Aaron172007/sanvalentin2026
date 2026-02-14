4279836828f26e058e80fc2d3560c884ddbe3dcf
Aaron172007 <proyectofinal585@gmail.com>
2026-02-13 23:56:55 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sanvalentin2026/'
   server: {
     host: true, // Permite acceder desde dispositivos móviles en la misma red
     port: 3000,
